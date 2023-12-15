b153d331e4462198bac7215bc53984ef125bae41
AndreNussbaumer <57812689+AndreNussbaumer@users.noreply.github.com>
2023-12-15 21:10:42 +0000

    Add files via upload

--- js/controls.js
+++ js/controls.js
@@ -86,7 +86,7 @@ class Joystick {
 }
 
 let positionJoystick = window.innerWidth / 9.1
-let positionJoyLeft = window.innerHeight / 1.11
+let positionJoyLeft = window.innerHeight / 1.05
 
 let ratio = window.innerHeight / window.innerWidth
 
