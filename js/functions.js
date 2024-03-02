d9259b2d6bb65d77df5410d2f25e6d6350ae8c81
AndreNussbaumer <57812689+AndreNussbaumer@users.noreply.github.com>
2024-03-02 12:46:51 +0000

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 function toggleFullscreen() {
-  if (!document.fullscreenElement) {
+  if(!document.fullscreenElement) {
     canvas.requestFullscreen().catch((err) => {
       alert(
         `Error attempting to enable fullscreen mode: ${err.message} (${err.name})`,
